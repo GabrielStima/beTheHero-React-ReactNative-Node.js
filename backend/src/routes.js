4f347a122b70cb41017c14ea7b2e6ceca20a58f7
Gabriel Stimamiglio <gabriel.stimamiglio@acertsis.com.br>
2020-03-28 16:54:09 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const routes = express.Router();
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+routes.get('/ongs', OngController.list)
+routes.get('/incidents', IncidentController.list)
+routes.get('/profile', ProfileController.list)
+
+routes.post('/ongs', OngController.create)
+routes.post('/incidents', IncidentController.create)
+routes.post('/session', SessionController.create)
+
+routes.delete('/incidents/:id', IncidentController.delete)
+routes.delete('/sessions', SessionController.delete)
+
+module.exports = routes;
